99e1ba308e2a9817ed90d728cd413488556e05f6
tejapullamsetty <60170098+tejapullamsetty@users.noreply.github.com>
2022-03-28 15:51:44 +0530

    Comitting the department microservice files

---
+++ department-service/src/main/java/com/example/department/repository/DepartmentRepository.java
@@ -0,0 +1,13 @@
+package com.example.department.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.department.entity.Department;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long>{
+
+	Department findByDepartmentId(Long departmentId);
+
+}
---
+++ department-service/src/main/java/com/example/department/service/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.example.department.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.department.entity.Department;
+import com.example.department.repository.DepartmentRepository;
+
+@Service
+public class DepartmentService {
+	
+	@Autowired
+	private DepartmentRepository departmentRepository;
+
+	public Department saveDepartment(Department department) {
+		
+		return departmentRepository.save(department);
+	}
+
+	public Department findDepartmentById(Long departmentId) {
+		
+		return departmentRepository.findByDepartmentId(departmentId);
+	}
+
+}
